fdff9df946e98251a64df7f157a44653148f55e6
ultravioletg <ultravioletrad@tutanota.com>
2023-05-16 07:55:40 +0500

    models and data

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Booking
+from .models import Menu
+
 # Register your models here.
+
+admin.site.register(Booking)
+admin.site.register(Menu)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from django.core.validators import MaxValueValidator
+
+
 
 # Create your models here.
+#for int(5) use:validators=[MaxValueValidator(99999)]
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
